370ce10ac916204db23325c2d5035d9a32f94d4c
Abe Jukema <abemj13@gmail.com>
2025-07-26 11:01:43 +0200

    made a player class and added a drawmethod for it to the main loop

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 
 def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    Running = True
+    running = True
 
-    pygame.init
+    pygame.init()
 
-    while Running:
-        
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while running:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill(black)
-        pygame.display.flip()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        player.draw(screen)
+
+        pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
